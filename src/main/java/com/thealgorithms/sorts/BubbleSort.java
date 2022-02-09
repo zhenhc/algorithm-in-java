7c345403ade0d243520afbd53eacb3ade505bc23
zhenhaochuang <zhenhaochuang@gmail.com>
2022-02-09 15:10:05 +0800

    冒泡排序

--- src/main/java/com/thealgorithms/sorts/BubbleSort.java
+++ src/main/java/com/thealgorithms/sorts/BubbleSort.java
@@ -34,13 +34,38 @@ class BubbleSort implements SortAlgorithm {
     }
 
     /**
+     * 从左到右不断交换相邻逆序的元素，在一轮的循环之后，可以让未排序的最大元素上浮到右侧。
+     * 在一轮循环中，如果没有发生交换，那么说明数组已经是有序的，此时可以直接退出。
+     * @param array
+     * @param <T>
+     * @return
+     */
+    public <T extends Comparable<T>> T[] sort1(T[] array){
+        for (int i=1,size = array.length;i<size;++i){
+            boolean swapped = false;
+            for (int j=0;j<size-i;++j){
+                //比较相邻两个元素，如果左边比右边大，那么就交换这两个元素
+                if (greater(array[j],array[j+1])){
+                    swap(array,j,j+1);
+                    swapped = true;
+                }
+            }
+            //在一轮循环中，如果变量swapped = false,说明没有发生交换，数组已经是有序的，直接break跳出循环。
+            if (!swapped){
+                break;
+            }
+        }
+        return array;
+    }
+
+    /**
      * Driver Code
      */
     public static void main(String[] args) {
 
         Integer[] integers = {4, 23, 6, 78, 1, 54, 231, 9, 12};
         BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.sort(integers);
+        bubbleSort.sort1(integers);
 
         for (int i = 0; i < integers.length - 1; ++i) {
             assert integers[i] <= integers[i + 1];
